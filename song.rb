af4c919e478ee3ac1e04ee55d7db68c387738301
OpenShift guest <core966@indamail.hu>
2013-04-28 18:44:57 -0400

    Added database config

--- song.rb
+++ song.rb
@@ -2,7 +2,11 @@ require 'dm-core'
 require 'dm-migrations'
 
 configure :development do
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
 configure do
